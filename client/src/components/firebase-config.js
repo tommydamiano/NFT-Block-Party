f631068ac86d490c572a0c3f8aa27820645f17ef
tommydamiano <59709543+tommydamiano@users.noreply.github.com>
2022-03-20 19:04:48 -0400

    Update firebase-config.js

--- client/src/components/firebase-config.js
+++ client/src/components/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-P6TIFTB3Dfnu-70UJdmlUDf3vaAKVdU",
-    authDomain: "nft-twitter-blocker.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     projectId: "nft-twitter-blocker",
     storageBucket: "nft-twitter-blocker.appspot.com",
     messagingSenderId: "947470924753",
-    appId: "1:947470924753:web:654cf49032d297a08d3ef0",
+    appId: "",
     measurementId: "G-PQN6JY4FE4"
   };
 
